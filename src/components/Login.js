921eb27c0607b678ea03463a1daf2ef10e3b5e4d
Ayon <ayonshafiul@users.noreply.github.com>
2021-06-23 08:11:26 +0600

    Fix hardcoded client id

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ const Login = (props) => {
   }
   return (
     <div>
-      <a href="https://todoist.com/oauth/authorize?client_id=595920ebda9e4f94bf996b1cb966164c&scope=data:read_write,data:delete,task:add,data:delete,project:delete&state=secretstring">
+      <a href={`https://todoist.com/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&scope=data:read_write,data:delete,task:add,data:delete,project:delete&state=secretstring`}>
         Login with todoist...
       </a>
     </div>
